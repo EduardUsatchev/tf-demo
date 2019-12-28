222864ce98bad86ebe5cf33e5e8a539fa83e536c
avielb <aviel33@gmail.com>
2019-12-28 10:12:54 +0200

    init commit

---
+++ ex1/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0b898040803850657"
+  instance_type = "t2.micro"
+#	tags {
+#    Name = "my ec2 instance"
+#  }
+}
