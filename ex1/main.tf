0366a903469e9fb144f04ba8b365ca25a668bd22
avielb <aviel33@gmail.com>
2023-10-30 21:49:08 +0200

    add stuff

--- ex1/main.tf
+++ ex1/main.tf
@@ -3,13 +3,25 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-0b898040803850657"
+  ami = "ami-0dbc3d7bc646e8516"
   instance_type = "t2.micro"
   tags = {
-    Name = "blabla"
+    Name = var.server_name
   }
 }
 
+#terraform {
+#  backend "s3" {
+#    region = "eu-central-1"
+#    bucket = "my-bucket-name"
+#    key = "path/to/state/file"
+#  }
+#}
+variable "server_name" {
+  type = string
+  default = "my-server"
+  description = "the name of the server"
+}
 output "ip_address" {
-  value = aws_instance.web.public_dns
+  value = aws_instance.web.private_ip
 }
